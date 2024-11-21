c5b4d75e0136b86b3a081e4cbd9ffd749ba97f56
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-11-21 17:11:05 +0100

    je backflip

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import os
+from picsellia import Client
+
+
 def main():
-    pass
+    client = Client(
+        api_token=os.getenv("PICSELLIA_USER_TOKEN"),
+        organization_name="Picsalex-MLOps",
+    )
+    dataset = client.get_dataset_by_id("01930b78-95d2-7ec5-9e09-3e2b00ee4af4")
+    print(dataset.name)
 
 
 if __name__ == "__main__":

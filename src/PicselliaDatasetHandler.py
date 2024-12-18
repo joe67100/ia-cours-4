f3ba9a6c791c9a8e197a75264d05fefb104a5a7e
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-12-18 16:33:32 +0100

    fix: use_id instead of img name

--- src/PicselliaDatasetHandler.py
+++ src/PicselliaDatasetHandler.py
@@ -1,5 +1,7 @@
 from .Abstract.DatasetHandler import DatasetHandler
 from picsellia.types.enums import AnnotationFileType
+from picsellia.sdk.dataset_version import DatasetVersion
+from picsellia import Client
 import os
 from dotenv import load_dotenv
 
@@ -7,20 +9,20 @@ load_dotenv()
 
 
 class PicselliaDatasetHandler(DatasetHandler):
-    def __init__(self, client):
+    def __init__(self, client: Client):
         self.client = client
         self.dataset_name = os.getenv("DATASET_NAME")
         self.dataset_version = os.getenv("DATASET_VERSION")
         self.annotation_output_path = os.getenv("ANNOTATION_OUTPUT_PATH")
-        self.dataset = self.client.get_dataset(self.dataset_name).get_version(
-            self.dataset_version
-        )
+        self.dataset: DatasetVersion = self.client.get_dataset(
+            self.dataset_name
+        ).get_version(self.dataset_version)
 
     def download_dataset(self):
         if not os.path.exists("./datasets"):
-            self.dataset.list_assets().download("./datasets")
+            self.dataset.list_assets().download("./datasets", use_id=True)
 
     def export_annotations(self):
         self.dataset.export_annotation_file(
-            AnnotationFileType.YOLO, self.annotation_output_path
+            AnnotationFileType.YOLO, self.annotation_output_path, use_id=True
         )
